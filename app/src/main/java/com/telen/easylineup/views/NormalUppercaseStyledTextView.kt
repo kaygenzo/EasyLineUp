eb9d8fd26c3dfd31794bafd44f7693ad3a13e192
Karim Yarboua <karim.yarboua@gmail.com>
2019-12-09 22:33:38 +0100

    ELU -- Change the style of team name for the normal uppercase one

--- app/src/main/java/com/telen/easylineup/views/NormalUppercaseStyledTextView.kt
+++ app/src/main/java/com/telen/easylineup/views/NormalUppercaseStyledTextView.kt
@@ -6,17 +6,24 @@ import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.content.ContextCompat
 import com.telen.easylineup.R
-import java.lang.StringBuilder
 
 class NormalUppercaseStyledTextView: AppCompatTextView {
 
-    constructor(context: Context) : super(context) {initView(context)}
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {initView(context)}
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {initView(context)}
+    constructor(context: Context) : super(context) {initView(context, null, 0)}
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {initView(context, attrs, 0)}
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {initView(context, attrs, defStyleAttr)}
 
-    private fun initView(context: Context) {
+    private fun initView(context: Context, attrs: AttributeSet?, defStyle: Int) {
         isAllCaps = true
-        setTextColor(ContextCompat.getColor(context, R.color.normal_uppercase_style_text_color))
+        val defaultColor = ContextCompat.getColor(context, R.color.normal_uppercase_style_text_color)
+        val textColor = attrs?.let {
+            val a = context.obtainStyledAttributes(attrs, R.styleable.NormalUppercaseStyledTextView, defStyle, 0)
+            val color = a.getColor(R.styleable.NormalUppercaseStyledTextView_normalUppercaseStyledTextColor, defaultColor)
+            a.recycle()
+            color
+        } ?: defaultColor
+
+        setTextColor(textColor)
     }
 
     override fun setTypeface(tf: Typeface?, style: Int) {
