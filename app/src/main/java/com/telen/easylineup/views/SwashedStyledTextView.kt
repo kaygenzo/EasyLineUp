6fd02392a9654950eadda65d6e31de1d9a83c1cc
Karim Yarboua <karim.yarboua@gmail.com>
2020-02-27 23:24:04 +0100

    ELU -- Adapt the font to the new name for splashscreen

--- app/src/main/java/com/telen/easylineup/views/SwashedStyledTextView.kt
+++ app/src/main/java/com/telen/easylineup/views/SwashedStyledTextView.kt
@@ -20,17 +20,17 @@ class SwashedStyledTextView: AppCompatTextView {
         super.setTypeface(Typeface.createFromAsset(context.assets, "KrinkesDecorPERSONAL.ttf"))
     }
 
-    override fun setText(text: CharSequence?, type: BufferType?) {
-        val builder = StringBuilder()
-        var textToSet = text
-        if(text != null) {
-            val builder = StringBuilder()
-            text.split(" ").forEach {
-                builder.append(it)
-                builder.append("9 ")
-            }
-            textToSet = builder.toString().trim()
-        }
-        super.setText(textToSet, type)
-    }
+//    override fun setText(text: CharSequence?, type: BufferType?) {
+//        val nameBuilder = StringBuilder()
+//        var textToSet = text
+//        if(text != null) {
+//            nameBuilder.append(textToSet).append('9')
+////            text.split(" ").forEach {
+////                builder.append(it)
+////                builder.append("9 ")
+////            }
+//            textToSet = nameBuilder.toString().trim()
+//        }
+//        super.setText(textToSet, type)
+//    }
 }
