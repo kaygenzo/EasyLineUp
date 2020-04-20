961adc0664be6276d0303a17a76e8a2c726a54ad
Karim Yarboua <karim.yarboua@gmail.com>
2020-04-20 22:54:31 +0200

    ELU -- Added missing files

---
+++ app/src/main/java/com/telen/easylineup/views/CustomEditTextView.kt
@@ -0,0 +1,26 @@
+package com.telen.easylineup.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.telen.easylineup.R
+import kotlinx.android.synthetic.main.view_edit_text_simple.view.*
+
+class CustomEditTextView: ConstraintLayout {
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.view_edit_text_simple, this)
+    }
+
+    fun getName() : String {
+        return textInputField.text.toString()
+    }
+
+    fun setHint(hint: String) {
+        textInputField.hint = hint
+    }
+}
