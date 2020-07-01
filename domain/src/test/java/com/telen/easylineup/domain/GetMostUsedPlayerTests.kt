28a06f62a0296b726e634fed1dbee4cedd6713bd
Karim Yarboua <karim.yarboua@gmail.com>
2020-07-01 23:42:34 +0200

    ELU -- Removed unused unit tests but kept the implementation to create in the future the all dashboard tiles one

--- domain/src/test/java/com/telen/easylineup/domain/GetMostUsedPlayerTests.kt
+++ domain/src/test/java/com/telen/easylineup/domain/GetMostUsedPlayerTests.kt
@@ -9,7 +9,6 @@ import com.telen.easylineup.domain.model.tiles.KEY_DATA_NAME
 import com.telen.easylineup.domain.model.tiles.KEY_DATA_SHIRT_NUMBER
 import com.telen.easylineup.domain.repository.PlayerFieldPositionRepository
 import com.telen.easylineup.domain.repository.PlayerRepository
-import com.telen.easylineup.domain.usecases.GetMostUsedPlayer
 import io.reactivex.Single
 import io.reactivex.observers.TestObserver
 import org.junit.Assert
@@ -22,60 +21,60 @@ import org.mockito.MockitoAnnotations
 import org.mockito.junit.MockitoJUnitRunner
 
 
-@RunWith(MockitoJUnitRunner::class)
-internal class GetMostUsedPlayerTests {
-
-    @Mock lateinit var playerDao: PlayerRepository
-    @Mock lateinit var playerFieldPositionsDao: PlayerFieldPositionRepository
-    lateinit var mGetMostUsedPlayer: GetMostUsedPlayer
-
-    lateinit var mPlayerGameCounts: MutableList<PlayerGamesCount>
-
-    @Before
-    fun init() {
-        MockitoAnnotations.initMocks(this)
-        mGetMostUsedPlayer = GetMostUsedPlayer(playerFieldPositionsDao, playerDao)
-
-        val player1 = PlayerGamesCount(playerID = 2L, size = 4)
-        val player2 = PlayerGamesCount(playerID = 4L, size = 3)
-        val player3 = PlayerGamesCount(playerID = 1L, size = 2)
-        val player4 = PlayerGamesCount(playerID = 3L, size = 1)
-
-        mPlayerGameCounts = arrayListOf(player1, player2, player3, player4)
-
-        Mockito.`when`(playerFieldPositionsDao.getMostUsedPlayers(1L)).thenReturn(Single.just(mPlayerGameCounts))
-        Mockito.`when`(playerDao.getPlayerByIdAsSingle(2L)).thenReturn(Single.just(Player(id = 2L, name = "toto", shirtNumber = 42, licenseNumber = 2L, teamId = 1L, image = "imagePath")))
-    }
-
-    @Test
-    fun shouldReturnNullElementIfListIsEmpty() {
-        Mockito.`when`(playerFieldPositionsDao.getMostUsedPlayers(1L)).thenReturn(Single.just(mutableListOf()))
-        val observer = TestObserver<GetMostUsedPlayer.ResponseValue>()
-        mGetMostUsedPlayer.executeUseCase(GetMostUsedPlayer.RequestValues(Team(id = 1L))).subscribe(observer)
-        observer.await()
-        observer.assertComplete()
-        Assert.assertEquals(null, observer.values().first().data)
-    }
-
-    @Test
-    fun shouldReturnDataWithFirstPlayerIfListIsNotEmpty() {
-        val observer = TestObserver<GetMostUsedPlayer.ResponseValue>()
-        mGetMostUsedPlayer.executeUseCase(GetMostUsedPlayer.RequestValues(Team(id = 1L))).subscribe(observer)
-        observer.await()
-        observer.assertComplete()
-        Assert.assertEquals("imagePath", observer.values().first().data?.getData()?.get(KEY_DATA_IMAGE))
-        Assert.assertEquals("toto", observer.values().first().data?.getData()?.get(KEY_DATA_NAME))
-        Assert.assertEquals(42, observer.values().first().data?.getData()?.get(KEY_DATA_SHIRT_NUMBER))
-        Assert.assertEquals(4, observer.values().first().data?.getData()?.get(KEY_DATA_MATCH_PLAYED))
-    }
-
-    @Test
-    fun shouldNotInsertImageIfPlayerImageIsNull() {
-        Mockito.`when`(playerDao.getPlayerByIdAsSingle(2L)).thenReturn(Single.just(Player(id = 2L, name = "toto", shirtNumber = 42, licenseNumber = 2L, teamId = 1L)))
-        val observer = TestObserver<GetMostUsedPlayer.ResponseValue>()
-        mGetMostUsedPlayer.executeUseCase(GetMostUsedPlayer.RequestValues(Team(id = 1L))).subscribe(observer)
-        observer.await()
-        observer.assertComplete()
-        Assert.assertEquals(null, observer.values().first().data?.getData()?.get(KEY_DATA_IMAGE))
-    }
-}
+//@RunWith(MockitoJUnitRunner::class)
+//internal class GetMostUsedPlayerTests {
+//
+//    @Mock lateinit var playerDao: PlayerRepository
+//    @Mock lateinit var playerFieldPositionsDao: PlayerFieldPositionRepository
+//    lateinit var mGetMostUsedPlayer: GetMostUsedPlayer
+//
+//    lateinit var mPlayerGameCounts: MutableList<PlayerGamesCount>
+//
+//    @Before
+//    fun init() {
+//        MockitoAnnotations.initMocks(this)
+//        mGetMostUsedPlayer = GetMostUsedPlayer(playerFieldPositionsDao, playerDao)
+//
+//        val player1 = PlayerGamesCount(playerID = 2L, size = 4)
+//        val player2 = PlayerGamesCount(playerID = 4L, size = 3)
+//        val player3 = PlayerGamesCount(playerID = 1L, size = 2)
+//        val player4 = PlayerGamesCount(playerID = 3L, size = 1)
+//
+//        mPlayerGameCounts = arrayListOf(player1, player2, player3, player4)
+//
+//        Mockito.`when`(playerFieldPositionsDao.getMostUsedPlayers(1L)).thenReturn(Single.just(mPlayerGameCounts))
+//        Mockito.`when`(playerDao.getPlayerByIdAsSingle(2L)).thenReturn(Single.just(Player(id = 2L, name = "toto", shirtNumber = 42, licenseNumber = 2L, teamId = 1L, image = "imagePath")))
+//    }
+//
+//    @Test
+//    fun shouldReturnNullElementIfListIsEmpty() {
+//        Mockito.`when`(playerFieldPositionsDao.getMostUsedPlayers(1L)).thenReturn(Single.just(mutableListOf()))
+//        val observer = TestObserver<GetMostUsedPlayer.ResponseValue>()
+//        mGetMostUsedPlayer.executeUseCase(GetMostUsedPlayer.RequestValues(Team(id = 1L))).subscribe(observer)
+//        observer.await()
+//        observer.assertComplete()
+//        Assert.assertEquals(null, observer.values().first().data)
+//    }
+//
+//    @Test
+//    fun shouldReturnDataWithFirstPlayerIfListIsNotEmpty() {
+//        val observer = TestObserver<GetMostUsedPlayer.ResponseValue>()
+//        mGetMostUsedPlayer.executeUseCase(GetMostUsedPlayer.RequestValues(Team(id = 1L))).subscribe(observer)
+//        observer.await()
+//        observer.assertComplete()
+//        Assert.assertEquals("imagePath", observer.values().first().data?.getData()?.get(KEY_DATA_IMAGE))
+//        Assert.assertEquals("toto", observer.values().first().data?.getData()?.get(KEY_DATA_NAME))
+//        Assert.assertEquals(42, observer.values().first().data?.getData()?.get(KEY_DATA_SHIRT_NUMBER))
+//        Assert.assertEquals(4, observer.values().first().data?.getData()?.get(KEY_DATA_MATCH_PLAYED))
+//    }
+//
+//    @Test
+//    fun shouldNotInsertImageIfPlayerImageIsNull() {
+//        Mockito.`when`(playerDao.getPlayerByIdAsSingle(2L)).thenReturn(Single.just(Player(id = 2L, name = "toto", shirtNumber = 42, licenseNumber = 2L, teamId = 1L)))
+//        val observer = TestObserver<GetMostUsedPlayer.ResponseValue>()
+//        mGetMostUsedPlayer.executeUseCase(GetMostUsedPlayer.RequestValues(Team(id = 1L))).subscribe(observer)
+//        observer.await()
+//        observer.assertComplete()
+//        Assert.assertEquals(null, observer.values().first().data?.getData()?.get(KEY_DATA_IMAGE))
+//    }
+//}
--- domain/src/test/java/com/telen/easylineup/domain/GetTeamSizeTests.kt
+++ domain/src/test/java/com/telen/easylineup/domain/GetTeamSizeTests.kt
@@ -5,7 +5,6 @@ import com.telen.easylineup.domain.model.Team
 import com.telen.easylineup.domain.model.tiles.KEY_DATA_SIZE
 import com.telen.easylineup.domain.model.tiles.KEY_DATA_TEAM_IMAGE
 import com.telen.easylineup.domain.repository.PlayerRepository
-import com.telen.easylineup.domain.usecases.GetTeamSize
 import io.reactivex.Single
 import io.reactivex.observers.TestObserver
 import org.junit.Assert
@@ -18,49 +17,49 @@ import org.mockito.MockitoAnnotations
 import org.mockito.junit.MockitoJUnitRunner
 
 
-@RunWith(MockitoJUnitRunner::class)
-internal class GetTeamSizeTests {
-
-    @Mock lateinit var playerDao: PlayerRepository
-    lateinit var mGetTeamSize: GetTeamSize
-    private var players = mutableListOf<Player>()
-    lateinit var team: Team
-
-    @Before
-    fun init() {
-        MockitoAnnotations.initMocks(this)
-        mGetTeamSize = GetTeamSize(playerDao)
-
-        Mockito.`when`(playerDao.getPlayers(1)).thenReturn(Single.just(players))
-        Mockito.`when`(playerDao.getPlayers(2)).thenReturn(Single.error(Exception()))
-
-        team = Team(1, "toto", "image", 0, false)
-
-        players.add(Player(1, 1, "", 1, 1, null, 1))
-        players.add(Player(2, 1, "", 2, 2, null, 1))
-        players.add(Player(3, 1, "", 3, 3, null, 1))
-        players.add(Player(4, 1, "", 4, 4, null, 1))
-        players.add(Player(5, 1, "", 5, 5, null, 1))
-    }
-
-    @Test
-    fun shouldTriggerAnExceptionIfTeamNotFound() {
-        val observer = TestObserver<GetTeamSize.ResponseValue>()
-        team.id = 2
-        mGetTeamSize.executeUseCase(GetTeamSize.RequestValues(team))
-                .subscribe(observer)
-        observer.await()
-        observer.assertError(Exception::class.java)
-    }
-
-    @Test
-    fun shouldReturnThePlayersSize() {
-        val observer = TestObserver<GetTeamSize.ResponseValue>()
-        mGetTeamSize.executeUseCase(GetTeamSize.RequestValues(team))
-                .subscribe(observer)
-        observer.await()
-        observer.assertComplete()
-        Assert.assertEquals(5, observer.values().first().data.getData()[KEY_DATA_SIZE] as? Int)
-        Assert.assertEquals("image", observer.values().first().data.getData()[KEY_DATA_TEAM_IMAGE] as? String)
-    }
-}
+//@RunWith(MockitoJUnitRunner::class)
+//internal class GetTeamSizeTests {
+//
+//    @Mock lateinit var playerDao: PlayerRepository
+//    lateinit var mGetTeamSize: GetTeamSize
+//    private var players = mutableListOf<Player>()
+//    lateinit var team: Team
+//
+//    @Before
+//    fun init() {
+//        MockitoAnnotations.initMocks(this)
+//        mGetTeamSize = GetTeamSize(playerDao)
+//
+//        Mockito.`when`(playerDao.getPlayers(1)).thenReturn(Single.just(players))
+//        Mockito.`when`(playerDao.getPlayers(2)).thenReturn(Single.error(Exception()))
+//
+//        team = Team(1, "toto", "image", 0, false)
+//
+//        players.add(Player(1, 1, "", 1, 1, null, 1))
+//        players.add(Player(2, 1, "", 2, 2, null, 1))
+//        players.add(Player(3, 1, "", 3, 3, null, 1))
+//        players.add(Player(4, 1, "", 4, 4, null, 1))
+//        players.add(Player(5, 1, "", 5, 5, null, 1))
+//    }
+//
+//    @Test
+//    fun shouldTriggerAnExceptionIfTeamNotFound() {
+//        val observer = TestObserver<GetTeamSize.ResponseValue>()
+//        team.id = 2
+//        mGetTeamSize.executeUseCase(GetTeamSize.RequestValues(team))
+//                .subscribe(observer)
+//        observer.await()
+//        observer.assertError(Exception::class.java)
+//    }
+//
+//    @Test
+//    fun shouldReturnThePlayersSize() {
+//        val observer = TestObserver<GetTeamSize.ResponseValue>()
+//        mGetTeamSize.executeUseCase(GetTeamSize.RequestValues(team))
+//                .subscribe(observer)
+//        observer.await()
+//        observer.assertComplete()
+//        Assert.assertEquals(5, observer.values().first().data.getData()[KEY_DATA_SIZE] as? Int)
+//        Assert.assertEquals("image", observer.values().first().data.getData()[KEY_DATA_TEAM_IMAGE] as? String)
+//    }
+//}
