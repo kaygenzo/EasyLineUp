a0eaed701974b408b3e4bf58d8890c5a8395cf4c
Karim Yarboua <karim.yarboua@famoco.com>
2023-07-14 18:05:11 +0200

    Transform utils in extension

--- app/src/main/java/com/telen/easylineup/utils/StringExt.kt
+++ app/src/main/java/com/telen/easylineup/utils/StringExt.kt
@@ -1,20 +1,18 @@
 package com.telen.easylineup.utils
 
-import java.util.*
+import java.util.Locale
 import java.util.regex.Pattern
 
-class StringUtils {
-    fun nameToLetters(name: String): String {
-        val parts: List<String> = name.trim().split(Pattern.compile("[\\s-]+"), 2).map {
-            if(it.isNotBlank())
-                it[0].toString().toUpperCase(Locale.ROOT)
-            else
-                "?"
-        }
-        val builder = StringBuilder()
-        parts.forEach {
-            builder.append(it)
-        }
-        return builder.toString()
+fun String.toLetters(): String {
+    val parts: List<String> = trim().split(Pattern.compile("[\\s-]+"), 2).map {
+        if (it.isNotBlank())
+            it[0].toString().toUpperCase(Locale.ROOT)
+        else
+            "?"
     }
+    val builder = StringBuilder()
+    parts.forEach {
+        builder.append(it)
+    }
+    return builder.toString()
 }
