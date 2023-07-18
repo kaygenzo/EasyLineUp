601c277eaa11c3189c61920dbd312414f823f625
Karim Yarboua <karim.yarboua@famoco.com>
2023-07-18 09:08:07 +0200

    Fix StringExt failing unit test

--- app/src/test/java/com/telen/easylineup/StringUtilsTest.kt
+++ app/src/test/java/com/telen/easylineup/StringUtilsTest.kt
@@ -1,39 +1,31 @@
 package com.telen.easylineup
 
-import com.telen.easylineup.utils.StringUtils
+import com.telen.easylineup.utils.toLetters
 import org.junit.Assert.assertEquals
-import org.junit.Before
 import org.junit.Test
 
 class StringUtilsTest {
 
-    lateinit var stringUtils: StringUtils
-
-    @Before
-    fun setup() {
-       stringUtils = StringUtils()
-    }
-
     @Test
     fun shouldEmptyStringReturnQuestionMarksString() {
-        assertEquals("?", stringUtils.nameToLetters(""))
-        assertEquals("?", stringUtils.nameToLetters(" "))
-        assertEquals("?", stringUtils.nameToLetters(" \n\r\n"))
+        assertEquals("?", "".toLetters())
+        assertEquals("?", " ".toLetters())
+        assertEquals("?", " \n\r\n".toLetters())
     }
 
     @Test
     fun shouldUniqueStringReturn1Letter() {
-        assertEquals(stringUtils.nameToLetters("Test"), "T")
+        assertEquals("T", "Test".toLetters(), )
     }
 
     @Test
     fun shouldComposedStringReturn2Letters() {
-        assertEquals(stringUtils.nameToLetters("Test Blabla"), "TB")
-        assertEquals(stringUtils.nameToLetters("Test-Blabla"), "TB")
+        assertEquals("TB", "Test Blabla".toLetters(), )
+        assertEquals("TB", "Test-Blabla".toLetters(), )
     }
 
     @Test
     fun shouldNameBetweenSpacesReturnInitial() {
-        assertEquals("L", stringUtils.nameToLetters("    Laura    "))
+        assertEquals("L", "    Laura    ".toLetters())
     }
 }
